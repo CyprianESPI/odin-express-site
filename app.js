23853de81100fde2223f6ff0c9f5f69e44caa620
CyprianESPI <cyprianespinoza@gmail.com>
2025-01-03 08:43:15 +0100

    Use .env file for PORT

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
+require('dotenv').config();
 const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => res.send("Hello, world!"));
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+const HOST_NAME = "localhost";
 app.listen(PORT, () => {
-  console.log(`My first Express app - listening on port ${PORT}!`);
+  console.log(`My first Express app - listening on port http://${HOST_NAME}:${PORT}!`);
 });
