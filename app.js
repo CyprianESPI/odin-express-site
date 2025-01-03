5a1f2ea22f6b7868debc638f2a43f23d0d41bf12
CyprianESPI <cyprianespinoza@gmail.com>
2025-01-03 09:05:11 +0100

    Send basic responses

--- app.js
+++ app.js
@@ -2,10 +2,18 @@ require('dotenv').config();
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello, world!"));
-
 const PORT = process.env.PORT || 3000;
 const HOST_NAME = "localhost";
+const ROUTES = [
+    { urlPath: '/', filePath: '/pages/index.html'},
+    { urlPath: '/about', filePath: '/pages/about.html'},
+    { urlPath: '/contact-me', filePath: '/pages/contact-me.html'},
+]
+
+ROUTES.forEach((route) => {
+    app.get(route.urlPath, (req, res) => res.sendFile(route.filePath, {root: '.'}));
+})
+
 app.listen(PORT, () => {
   console.log(`My first Express app - listening on port http://${HOST_NAME}:${PORT}!`);
 });
